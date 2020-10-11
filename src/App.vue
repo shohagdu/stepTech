6c28b814fd46387f269058718e11411776493890
shohagdu <omarshohag93@gmail.com>
2020-10-11 23:17:08 +0600

    finally update

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="/assets/img/navbar-logo.svg" alt="" /> Step Tech</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="./assets/img/logo.png" alt="Step Tech" style="height: 60px !important;padding: 0px !important;margin: 0px" ></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars ml-1"></i>
@@ -22,7 +22,7 @@
     <!-- Masthead-->
     <header class="masthead">
       <div class="container">
-        <div class="masthead-subheading">Welcome To Our Studio!</div>
+        <div class="masthead-subheading">Welcome To Step Technology</div>
         <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
         <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
       </div>
@@ -32,7 +32,7 @@
       <div class="container">
         <div class="text-center">
           <h2 class="section-heading text-uppercase">Services</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 class="section-subheading text-muted"> Best Web Application Development in Bangladesh</h3>
         </div>
         <div class="row text-center">
           <div class="col-md-4">
@@ -41,7 +41,7 @@
                             <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
                         </span>
             <h4 class="my-3">Web Development</h4>
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <p class="text-justify">Step Tech web application development practice addresses a wide range of business needs. Whether it's a content management system or a web-based data interface, our solutions demonstrate all the hallmarks .  <a href="#"> Read More..</a> </p>
           </div>
           <div class="col-md-4">
                         <span class="fa-stack fa-4x">
@@ -49,7 +49,7 @@
                             <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
                         </span>
             <h4 class="my-3">Responsive Design</h4>
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <p class="text-justify">In the field of Web design , we’re quickly getting to the point of being unable to keep up with the endless new resolutions and devices. For many websites, creating a website version for each resolution   <a href="#"> Read More..</a> </p>
           </div>
           <div class="col-md-4">
                         <span class="fa-stack fa-4x">
@@ -57,7 +57,7 @@
                             <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
                         </span>
             <h4 class="my-3">E-Commerce</h4>
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <p class="text-justify">As more and more customers are going (and staying) online, it is crucial that your website is fully equipped to handle their widening range of expectations and needs. Whether you need to build a new website experience <a href="#"> Read More..</a></p>
           </div>
 
 
@@ -69,7 +69,7 @@
       <div class="container">
         <div class="text-center">
           <h2 class="section-heading text-uppercase">Portfolio</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 class="section-subheading text-muted">Step Tech working lots of project.</h3>
         </div>
         <div class="row">
           <div class="col-lg-4 col-sm-6 mb-4">
@@ -78,11 +78,11 @@
                 <div class="portfolio-hover">
                   <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
-                <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="" />
+                <img class="img-fluid protfolio_image" src="./assets/img/portfolio/real-easte.png" alt=""  />
               </a>
               <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">Threads</div>
-                <div class="portfolio-caption-subheading text-muted">Illustration</div>
+                <div class="portfolio-caption-heading">Online Admission</div>
+                <div class="portfolio-caption-subheading text-muted">Online Registration, Application & Result Process</div>
               </div>
             </div>
           </div>
@@ -92,11 +92,11 @@
                 <div class="portfolio-hover">
                   <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
-                <img class="img-fluid" src="assets/img/portfolio/02-thumbnail.jpg" alt="" />
+                <img class="img-fluid protfolio_image" src="./assets/img/portfolio/up_automation.png" alt="" />
               </a>
               <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">Explore</div>
-                <div class="portfolio-caption-subheading text-muted">Graphic Design</div>
+                <div class="portfolio-caption-heading">UP Automation</div>
+                <div class="portfolio-caption-subheading text-muted">Union Parisad Automation software </div>
               </div>
             </div>
           </div>
@@ -106,11 +106,11 @@
                 <div class="portfolio-hover">
                   <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
-                <img class="img-fluid" src="assets/img/portfolio/03-thumbnail.jpg" alt="" />
+                <img class="img-fluid protfolio_image" src="./assets/img/portfolio/hospital_website.png" alt="" />
               </a>
               <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">Finish</div>
-                <div class="portfolio-caption-subheading text-muted">Identity</div>
+                <div class="portfolio-caption-heading">Hospital Website</div>
+                <div class="portfolio-caption-subheading text-muted">Hospital Website and Management System </div>
               </div>
             </div>
           </div>
@@ -120,11 +120,11 @@
                 <div class="portfolio-hover">
                   <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
-                <img class="img-fluid" src="assets/img/portfolio/04-thumbnail.jpg" alt="" />
+                <img class="img-fluid protfolio_image" src="./assets/img/portfolio/du_club.png" alt="" />
               </a>
               <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">Lines</div>
-                <div class="portfolio-caption-subheading text-muted">Branding</div>
+                <div class="portfolio-caption-heading">Club Management</div>
+                <div class="portfolio-caption-subheading text-muted">Member & Accounting Manage.</div>
               </div>
             </div>
           </div>
@@ -134,11 +134,11 @@
                 <div class="portfolio-hover">
                   <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
-                <img class="img-fluid" src="assets/img/portfolio/05-thumbnail.jpg" alt="" />
+                <img class="img-fluid protfolio_image" src="./assets/img/portfolio/inventory_accounting.jpg" alt="" />
               </a>
               <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">Southwest</div>
-                <div class="portfolio-caption-subheading text-muted">Website Design</div>
+                <div class="portfolio-caption-heading">Invantory & Accounting</div>
+                <div class="portfolio-caption-subheading text-muted">Inventory Stock & Stock Management </div>
               </div>
             </div>
           </div>
@@ -148,11 +148,11 @@
                 <div class="portfolio-hover">
                   <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
-                <img class="img-fluid" src="assets/img/portfolio/06-thumbnail.jpg" alt="" />
+                <img class="img-fluid protfolio_image" src="./assets/img/portfolio/construction_company.jpg" alt="" />
               </a>
               <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">Window</div>
-                <div class="portfolio-caption-subheading text-muted">Photography</div>
+                <div class="portfolio-caption-heading">Construction Manage</div>
+                <div class="portfolio-caption-subheading text-muted">Construction Company Project Management</div>
               </div>
             </div>
           </div>
@@ -164,14 +164,14 @@
       <div class="container">
         <div class="text-center">
           <h2 class="section-heading text-uppercase">About</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 class="section-subheading text-muted">Step Tech is the best software firms in Bangladesh</h3>
         </div>
         <ul class="timeline">
           <li>
             <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="" /></div>
             <div class="timeline-panel">
               <div class="timeline-heading">
-                <h4>2009-2011</h4>
+                <h4>2016-2017</h4>
                 <h4 class="subheading">Our Humble Beginnings</h4>
               </div>
               <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
@@ -181,7 +181,7 @@
             <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/2.jpg" alt="" /></div>
             <div class="timeline-panel">
               <div class="timeline-heading">
-                <h4>March 2011</h4>
+                <h4>March 2018</h4>
                 <h4 class="subheading">An Agency is Born</h4>
               </div>
               <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
@@ -191,22 +191,13 @@
             <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="" /></div>
             <div class="timeline-panel">
               <div class="timeline-heading">
-                <h4>December 2012</h4>
+                <h4>December 2019</h4>
                 <h4 class="subheading">Transition to Full Service</h4>
               </div>
               <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
             </div>
           </li>
-          <li class="timeline-inverted">
-            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="" /></div>
-            <div class="timeline-panel">
-              <div class="timeline-heading">
-                <h4>July 2014</h4>
-                <h4 class="subheading">Phase Two Expansion</h4>
-              </div>
-              <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-            </div>
-          </li>
+          
           <li class="timeline-inverted">
             <div class="timeline-image">
               <h4>
@@ -247,43 +238,69 @@
       <div class="container">
         <div class="text-center">
           <h2 class="section-heading text-uppercase">Contact Us</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 class="section-subheading text-muted">Have Any query, Please contact</h3>
         </div>
-        <form id="contactForm" name="sentMessage" novalidate="novalidate">
-          <div class="row align-items-stretch mb-5">
-            <div class="col-md-6">
-              <div class="form-group">
-                <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
-                <p class="help-block text-danger"></p>
+        <div class="col-md-12">
+            <form id="contactForm" name="sentMessage" novalidate="novalidate">
+              <div class="row align-items-stretch mb-5">
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                    <p class="help-block text-danger"></p>
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                    <p class="help-block text-danger"></p>
+                  </div>
+                  <div class="form-group mb-md-0">
+                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="form-group form-group-textarea mb-md-0">
+                    <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div class="col-md-4 contact-text">
+
+<!--                  <div >-->
+<!--                    <a class="footer_maps" href="https://www.google.com.bd/maps/@23.8043848,90.4154625,17.5z"-->
+<!--                       target="_blank"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.0023198424424!2d90.4002231149988!3d23.711611196173813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b91cdaf6aed7%3A0xd73ea99fcc43b05d!2sMedilife+Specialized+Hospital+Ltd!5e0!3m2!1sbn!2sbd!4v1558985870459!5m2!1sbn!2sbd" width="100%" height="250px"  frameborder="0" style="border:0" allowfullscreen></iframe></a>-->
+<!--                  </div>-->
+
+                  <div class="contact-main-info text-left">
+                    <div class="margin-top-20px"></div>
+                    <div class="clearfix"></div>
+                    +880 1672-758084
+                    <div class="clearfix"></div>
+                    +880 1673-050507
+                    <div class="clearfix"></div>
+                    <div class="margin-top-20px"></div>
+                    info@steptechbd.com
+                    <div class="margin-top-20px"></div>
+                    Nilkhet Rd, Dhaka 1000
+                  </div>
+
+                </div>
+
               </div>
-              <div class="form-group">
-                <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
-                <p class="help-block text-danger"></p>
+              <div class="col-md-8">
+                <div class="text-center">
+                  <div id="success"></div>
+                  <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
+                </div>
               </div>
-              <div class="form-group mb-md-0">
-                <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group form-group-textarea mb-md-0">
-                <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-          </div>
-          <div class="text-center">
-            <div id="success"></div>
-            <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
-          </div>
-        </form>
+            </form>
+        </div>
       </div>
     </section>
     <!-- Footer-->
     <footer class="footer py-4">
       <div class="container">
         <div class="row align-items-center">
-          <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+          <div class="col-lg-4 text-lg-left">Copyright © team steptech</div>
           <div class="col-lg-4 my-3 my-lg-0">
             <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
             <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
@@ -497,7 +514,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  /*margin-top: 60px;*/
+  margin-top: 60px;
 }
+#mainNav{
+  background: #2c3e50 !important;
+}
+.protfolio_image{
+  height: 300px !important;
+}
+.contact-text{
+  color:white;
 
+}
+.contact-main-info{
+  margin-left: 50px;
+}
+.margin-top-20px{
+  margin-top:20px;
+}
 </style>
